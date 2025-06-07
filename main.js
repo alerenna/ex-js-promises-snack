e6bc61bba8f6879a77c3716cfa3fc4c8bde16063
“alerenna” <alessandrorennafm@gmail.com>
2025-06-07 17:41:53 +0200

    Refactor getPostId and getPostTitle functions for clarity and consistency. Add getPostId with Author

--- main.js
+++ main.js
@@ -4,13 +4,30 @@ Ottieni il titolo di un post con una Promise.
 
 Crea una funzione getPostTitle(id) che accetta un id e restituisce una Promise che recupera il titolo di un post dal link https://dummyjson.com/posts/{id}
 🎯 Bonus: Ottieni l'intero post con l'autore
-Crea una funzione getPost(id) che recupera l'intero post. Concatena una seconda chiamata che aggiunge una proprietà user che contiene i dati dell'autore, recuperati dalla chiamata https://dummyjson.com/users/{post.userId}.
+Crea una funzione getPost(id) che recupera l'intero post.
+Concatena una seconda chiamata che aggiunge una proprietà user che contiene i dati dell'autore, recuperati dalla chiamata https://dummyjson.com/users/{post.userId}.
 */
 
-const getPostId = id => {
+const getPostTitle = id => {
 
     const promessa = new Promise((resolve, reject) => {
         fetch(`https://dummyjson.com/posts/${id}`)
+            .then(response => response.json())
+            .then(obj => resolve(obj.title))
+            .catch(reject)
+    })
+
+    return promessa
+}
+
+const getPostId = id => {
+    const promessa = new Promise((resolve, reject) => {
+        fetch(`https://dummyjson.com/posts/${id}`)
+            .then(response => response.json())
+            .then(post => {
+                console.log("Post:", post);
+                return fetch(`https://dummyjson.com/users/${post.userId}`)
+            })
             .then(response => response.json())
             .then(obj => resolve(obj))
             .catch(reject)
@@ -19,6 +36,11 @@ const getPostId = id => {
     return promessa
 }
 
-getPostId(1)
-    .then(obj => console.log(obj.title))
+getPostTitle(1)
+    .then(obj => console.log(obj))
     .catch(error => console.error(error))
+
+getPostId(1)
+    .then(obj => console.log("Author:", obj))
+    .catch(error => console.log(error))
+
