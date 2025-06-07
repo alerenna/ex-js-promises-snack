cc7415709f1819193685979eff4290b7fbc60d3a
“alerenna” <alessandrorennafm@gmail.com>
2025-06-07 18:52:19 +0200

    Refactor lanciaDado function to creaLanciaDado with closure for last roll tracking

--- snack2-bonus/main.js
+++ snack2-bonus/main.js
@@ -6,7 +6,7 @@ Crea la funzione lanciaDado() che restituisce una Promise che, dopo 3 secondi, g
 Modifica la funzione in creaLanciaDado(), che restituisce una closure che memorizza l'ultimo risultato. Se il numero esce due volte di fila, stampa "Incredibile!".
 */
 
-function lanciaDado() {
+/* function lanciaDado() {
     return new Promise((resolve, reject) => {
         console.log("Sto lanciando il dado...");
         setTimeout(() => {
@@ -22,7 +22,50 @@ function lanciaDado() {
 
 lanciaDado()
     .then(messaggio => console.log(messaggio))
-    .catch(error => console.log(error))
+    .catch(error => console.log(error)) */
 
+function creaLanciaDado() {
+
+    let ultimoLancio = 0
+
+    return function lancia() {
+        return new Promise((resolve, reject) => {
+            console.log("Sto lanciando il dado...");
+            setTimeout(() => {
+                if (Math.random() < 0.2) {
+                    reject("Ops, il dado si è incastrato");
+                    return
+                }
+                const valore = Math.floor(Math.random() * 6) + 1
+                if (valore === ultimoLancio) {
+                    resolve(`Incredibile è uscito di nuovo il ${valore}!`)
+                } else {
+                    resolve(`Avanza di ${valore} caselle`)
+                }
+                ultimoLancio = valore
+            }, 1000)
+        })
+    }
+
+
+}
+
+const lanciaDado = creaLanciaDado()
+function ripetiLancio(n) {
+    if (n === 0) return
+
+    lanciaDado()
+        .then(messaggio => {
+            console.log(messaggio)
+            ripetiLancio(n - 1)
+        })
+        .catch(error => {
+            console.log(error);
+            ripetiLancio(n - 1);
+        })
+
+}
+
+ripetiLancio(10)
 
 
